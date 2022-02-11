b0013bfefc9f103d981d85bf440eb87d99258431
aniltheeng <7aniltheeng@gmail.com>
2022-02-11 18:53:25 +0545

    first commit

---
+++ frontend/src/component/Cart/Payment.js
@@ -0,0 +1,61 @@
+import React, { Fragment, useEffect, useRef } from "react";
+import CheckoutSteps from "../Cart/CheckoutSteps";
+import { useSelector, useDispatch } from "react-redux";
+import MetaData from "../layout/MetaData";
+import { Typography } from "@material-ui/core";
+import { useAlert } from "react-alert";
+import "./payment.css";
+import { createOrder, clearErrors } from "../../actions/orderAction";
+
+const Payment = ({ history }) => {
+  const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo"));
+
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const payBtn = useRef(null);
+
+  const { shippingInfo, cartItems } = useSelector((state) => state.cart);
+  const { error } = useSelector((state) => state.newOrder);
+
+  const order = {
+    shippingInfo,
+    orderItems: cartItems,
+    itemsPrice: orderInfo.subtotal,
+    taxPrice: orderInfo.tax,
+    shippingPrice: orderInfo.shippingCharges,
+    totalPrice: orderInfo.totalPrice,
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+          dispatch(createOrder(order));
+          history.push("/success");
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+  }, [dispatch, error, alert]);
+
+  return (
+    <Fragment>
+      <MetaData title="Payment" />
+      <CheckoutSteps activeStep={2} />
+      <div className="paymentContainer">
+        <form className="paymentForm" onSubmit={(e) => submitHandler(e)}>
+          <Typography>Card Info</Typography>
+          <input
+            type="submit"
+            value={`Pay - ₹${orderInfo && orderInfo.totalPrice}`}
+            ref={payBtn}
+            className="paymentFormBtn"
+          />
+        </form>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Payment;
